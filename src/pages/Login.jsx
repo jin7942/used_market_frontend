201d6f75eff4b719a92a8396fb3dca6d625e2a20
김진범 <infomer.jin@gmail.com>
2025-03-26 02:24:57 +0900

    1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,41 @@
 // 로그인 모달
 import propTypes from 'prop-types';
 import { Link } from 'react-router';
+import util from '../common/util';
+import { useState } from 'react';
 
 const Login = ({ isOpen, onClose }) => {
+    const [formData, setFormData] = useState({
+        userEmail: '',
+        userPassword: '',
+    });
+
     if (!isOpen) return null;
+
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        // 유효성 검사
+        const err1 = util.validateField('userEmail', formData.userEmail, formData);
+        const err2 = util.validateField('userPassword', formData.userPassword, formData);
+
+        if (err1 || err2) {
+            alert(err1 || err2);
+            return;
+        }
+
+        // 정상값이면 로그인 요청
+        const res = await util.loginUser(formData.userEmail, formData.userPassword);
+        if (res.success === true) {
+            onClose();
+        } else {
+            alert(res.err);
+        }
+    };
 
     return (
         <div className={`modal fade ${isOpen ? 'show d-block' : 'd-none'}`} tabIndex='-1' role='dialog' aria-modal='true'>
@@ -22,11 +54,11 @@ const Login = ({ isOpen, onClose }) => {
                             <form>
                                 {/* input for Email */}
                                 <label className='form-label text-start d-block mb-3'>Email</label>
-                                <input type='text' className='form-control' />
+                                <input type='text' className='form-control' name='userEmail' value={formData.useremail} onChange={onChange} />
 
                                 {/* input for Password */}
                                 <label className='form-label text-start d-block mb-3'>Password</label>
-                                <input type='password' className='form-control mb-3' />
+                                <input type='password' className='form-control mb-3' name='userPassword' value={formData.userPassword} onChange={onChange} />
 
                                 <hr />
                                 <button className='form-control d-block btn btn-success mb-3'>NAVER</button>
@@ -43,7 +75,7 @@ const Login = ({ isOpen, onClose }) => {
                             <button type='button' className='btn btn-secondary' onClick={onClose}>
                                 Close
                             </button>
-                            <button type='button' className='btn btn-success'>
+                            <button type='button' className='btn btn-success' onClick={handleLogin}>
                                 Login
                             </button>
                         </div>
