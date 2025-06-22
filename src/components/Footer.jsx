355e3e628e81c328a1fdf26412ef0251629d296b
jin7742 <infomer.jin@gmail.com>
2025-06-23 01:59:57 +0900

    feat: Change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,22 @@
 const Footer = () => {
     return (
-        <footer className='bg-dark text-white py-3 mt-auto'>
-            <div className='container'>
-                <div className='row align-items-center'>
-                    <div className='col-3'>
-                        <img src='/img/logo.webp' alt='Company Logo' className='mb-3' style={{ width: '180px', height: 'auto' }} />
+        <footer className="bg-dark text-white py-3 mt-auto">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-3">
+                        <img
+                            src="/img/logo.webp"
+                            alt="Company Logo"
+                            className="mb-3"
+                            style={{ width: '180px', height: 'auto' }}
+                        />
                     </div>
-                    <div className='col-9 text-center'>
-                        <p>© 2025 Your Company Name. All rights reserved.</p>
-                        <p>1234 Main St, Anytown, USA</p>
-                        <p>Email: info@yourcompany.com | Phone: (123) 456-7890</p>
+                    <div className="col-9 text-center">
+                        <p>JINBEOM KIM</p>
+                        <p>
+                            GitHub: <a href="https://github.com/jin7942">바로가기</a>
+                        </p>
+                        <p>Email: jinbeom.kim01@gmail.com</p>
                     </div>
                 </div>
             </div>
