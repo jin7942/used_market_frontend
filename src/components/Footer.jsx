c9e2383b71282580b510afb0bf6d97f047b57fa8
jin7742 <infomer.jin@gmail.com>
2025-07-01 01:55:55 +0900

    feat: Add alert for dumy data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,34 @@
 const Footer = () => {
     return (
-        <footer className="bg-dark text-white py-3 mt-auto">
-            <div className="container">
-                <div className="row align-items-center">
-                    <div className="col-3">
-                        <img
-                            src="/img/logo.webp"
-                            alt="Company Logo"
-                            className="mb-3"
-                            style={{ width: '180px', height: 'auto' }}
-                        />
-                    </div>
-                    <div className="col-9 text-center">
-                        <p>JINBEOM KIM</p>
-                        <p>
-                            GitHub: <a href="https://github.com/jin7942">바로가기</a>
-                        </p>
-                        <p>Email: jinbeom.kim01@gmail.com</p>
-                    </div>
+        <>
+            <div className="container mt-5">
+                <div className="alert alert-warning text-center fw-bold" role="alert">
+                    본 페이지의 모든 컨텐츠는 프로젝트 시연을 위한 더미 데이터이며, 실제 판매 상품이
+                    아닙니다.
                 </div>
             </div>
-        </footer>
+            <footer className="bg-dark text-white py-3 mt-auto">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-3">
+                            <img
+                                src="/img/logo.webp"
+                                alt="Company Logo"
+                                className="mb-3"
+                                style={{ width: '180px', height: 'auto' }}
+                            />
+                        </div>
+                        <div className="col-9 text-center">
+                            <p>JINBEOM KIM</p>
+                            <p>
+                                GitHub: <a href="https://github.com/jin7942">바로가기</a>
+                            </p>
+                            <p>Email: jinbeom.kim01@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
     );
 };
 
