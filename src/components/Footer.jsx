6612f17cdf9db153d17ba3fd932d3603c3919f0a
jin7742 <infomer.jin@gmail.com>
2025-07-02 00:06:36 +0900

    style: Align footer item center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,21 +10,23 @@ const Footer = () => {
             <footer className="bg-dark text-white py-3 mt-auto">
                 <div className="container">
                     <div className="row align-items-center">
-                        <div className="col-3">
-                            {/* <img
+                        {/* <div className="col-3">
+                            <img
                                 src="/img/logo.webp"
                                 alt="Company Logo"
                                 className="mb-3"
                                 style={{ width: '180px', height: 'auto' }}
-                            /> */}
-                        </div>
-                        <div className="col-9 text-center">
+                            />
+                        </div> */}
+                        <div className="col-3"></div>
+                        <div className="col-6 text-center">
                             <p>JINBEOM KIM</p>
                             <p>
                                 GitHub: <a href="https://github.com/jin7942">바로가기</a>
                             </p>
                             <p>Email: jinbeom.kim01@gmail.com</p>
                         </div>
+                        <div className="col-3"></div>
                     </div>
                 </div>
             </footer>
